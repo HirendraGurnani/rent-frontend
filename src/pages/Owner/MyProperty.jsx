500dcea1ef1f67ac5841dcd028adf8fcb8cf017f
HirendraGurnani <ghiren20@gmail.com>
2025-06-11 20:26:43 +0530

    property page import corrected

--- src/pages/Owner/MyProperty.jsx
+++ src/pages/Owner/MyProperty.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import OwnerSideBar from "../../component/OwnerSidebar";
+import OwnerSideBar from "../../component/OwnerSideBar";
 import axios from "axios";
 import Slider from "../../component/Slider";
 
